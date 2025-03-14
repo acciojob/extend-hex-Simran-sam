6d8c4122b622e9965db4388628b33c44a9af608c
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-14 15:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let code='#'+ shortHex
+    .slice(shortHex.startsWith('#') ? 1 : 0)
+    .split('')
+    .map(x => x + x)
+    .join('');
+	return code;
 };
 
 // Do not change the code below.
